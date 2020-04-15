3a864bee2d1b156a9de2cb7b00ccdbdb4a0f2a98
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-15 12:04:38 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -9,6 +9,9 @@ describe "#introduction" do
   end
 end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 describe "#introduction_with_language" do 
   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
     expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
